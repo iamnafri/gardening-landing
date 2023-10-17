6cac225ad7ca465e9fb88330b82a9e8304a77208
Irfan <sikumbangirfann@gmail.com>
2023-10-18 01:57:39 +0800

    chore: unistall `postcss-prune-var` package

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,5 @@
-const plugins = {
-  "@pandacss/dev/postcss": {},
-  ...(process.env.NODE_ENV === "production" ? { "postcss-prune-var": {} } : {}),
-};
-
-console.log("process.env.NODE_ENV : ", process.env.NODE_ENV);
-console.log({ plugins });
-
 module.exports = {
-  plugins,
+  plugins: {
+    "@pandacss/dev/postcss": {},
+  },
 };
