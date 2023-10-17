79b42951d99b27bdf8e9841424fdc985ee8fb036
Irfan <sikumbangirfann@gmail.com>
2023-10-18 01:18:30 +0800

    chore: optimize image size and remove unused CSS vars

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
-  plugins: [
-    "@pandacss/dev/postcss",
-    process.env.NODE_ENV === "production" ? "postcss-prune-var" : undefined,
-  ],
+  plugins: {
+    "@pandacss/dev/postcss": {},
+    ...(process.env.NODE_ENV === "production"
+      ? { "postcss-prune-var": {} }
+      : {}),
+  },
 };
